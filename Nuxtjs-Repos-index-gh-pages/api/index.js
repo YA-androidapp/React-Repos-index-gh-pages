98a173aa3f8263546854577ab71b0bb55ff4cdcf
YA <YA-androidapp@users.noreply.github.com>
2019-11-16 16:27:40 +0900

    Add a POST route
    $ npm install --save express body-parser

--- Nuxtjs-Repos-index-gh-pages/api/index.js
+++ Nuxtjs-Repos-index-gh-pages/api/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+// POST data
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+// POST data
+
 const app = express()
 router.use((req, res, next) => {
   Object.setPrototypeOf(req, app.request)
@@ -17,10 +22,18 @@ router.get("/", (req, res) => {
 });
 
 router.get("/site/:id", (req, res) => {
-  console.log("id:", req.params.id)
+  console.log("id(get):", req.params.id)
   res.status(200).json({
     message: "Success",
     id: `${req.params.id}`
+  });
+});
+
+router.post("/site", jsonParser, (req, res, nuxt) => {
+  console.log("id(post):", req.body.id)
+  res.status(200).json({
+    message: "Success",
+    id: `${req.body.id}`
   });
 });
 
